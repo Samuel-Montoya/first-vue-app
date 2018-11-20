a3ee3af43f9d6180268254b8684108e31f282e6f
TOG-Group <samuel@ostergroup.com>
2018-11-20 10:51:18 -0700

    adding to repo

---
+++ pages/index.vue
@@ -0,0 +1,148 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        your portal
+      </h1>
+      <h2 class="subtitle">
+        welcome to tog
+      </h2>
+      <div class="links">
+        <button 
+          class="button--green" 
+          @click="handleClick('register')">Create Account</button>
+        <button 
+          class="button--grey" 
+          @click="handleClick('login')">Login</button>
+      </div>
+
+      <div v-if="loginType === 'login'">
+        <h1 class="title is-3">Login</h1>
+        <h2 class="subtitle is-6">enter your username and password</h2>
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input 
+              v-model="username"
+              class="input"
+              placeholder="Username"
+              @change="checkUsername()">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"/>
+            </span>
+            <span 
+              v-if="isValid" 
+              class="icon is-small is-right">
+              <i 
+                class="fas fa-check" 
+                style="color: green"/>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input 
+              class="input" 
+              type="password" 
+              placeholder="Password">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"/>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div v-else>
+        <h1 class="title is-3">Register</h1>
+        <h2 class="subtitle is-6">create your username and password</h2>
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input 
+              v-model="username"
+              class="input"
+              placeholder="Username"
+              @change="checkUsername()">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"/>
+            </span>
+            <span 
+              v-if="isValid" 
+              class="icon is-small is-right">
+              <i 
+                class="fas fa-check" 
+                style="color: green"/>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input 
+              class="input" 
+              type="password" 
+              placeholder="Password">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"/>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  asyncData() {
+    return { loginType: 'login', isValid: false }
+  },
+  components: {
+    Logo
+  },
+  methods: {
+    handleClick(mode) {
+      this.loginType = mode
+    },
+    checkUsername() {
+      if(this.username.length){
+        this.isValid = true
+      } else {
+        this.isValid = false
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 5px;
+}
+
+.links {
+  padding-top: 5px;
+  margin-bottom: 50px;
+}
+</style>
---
+++ pages/profile/_profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Your Profile</h1>
+    <p>{{ firstName }}</p>
+  </div>
+</template>
+
+<script> 
+function getUserData () {
+  const userName = 'Jam'
+  return new Promise(resolve => {
+    return setTimeout(() => { return resolve(userName) }, 1000)
+  })
+}
+getUserData().then(data => console.log(data))
+export default {
+  async asyncData() {
+    const firstName = await getUserData()
+    return { firstName }
+  },
+  head() {
+    return { title: this.firstName + ' ass' }
+  }
+}
+</script>
